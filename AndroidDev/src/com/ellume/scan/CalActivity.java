bbda941c36544886532f5ec889342579ae984746
Spencer <spencervd6@gmail.com>
2014-05-23 16:33:53 -0700

    Spencer first commit for the merge

---
+++ AndroidDev/src/com/ellume/scan/CalActivity.java
@@ -0,0 +1,25 @@
+package com.ellume.scan;
+
+import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CalActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cal);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		this.getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		this.getActionBar().setCustomView(R.layout.menu_cal);
+		getMenuInflater().inflate(R.menu.cal, menu);
+		return true;
+	}
+
+}
--- AndroidDev/src/com/ellume/scan/CalendarV.java
+++ AndroidDev/src/com/ellume/scan/CalendarV.java
@@ -1,4 +1,4 @@
-package com.example.androiddev;
+package com.ellume.scan;
 
 import java.util.ArrayList;
 import java.util.Calendar;
