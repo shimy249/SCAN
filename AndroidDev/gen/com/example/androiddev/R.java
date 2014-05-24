c71741da68fc49bccb6b55168c9386a5d402122e
Stephanie <Stephanie@Stephanie-THINK>
2014-05-23 17:51:50 -0700

    git ignore

--- AndroidDev/gen/com/example/androiddev/R.java
+++ AndroidDev/gen/com/example/androiddev/R.java
@@ -8,6 +8,10 @@
 package com.example.androiddev;
 
 public final class R {
+    public static final class array {
+        public static final int listArray=0x7f050000;
+        public static final int listValues=0x7f050001;
+    }
     public static final class attr {
         /** <p>Must be one of the following constant values.</p>
 <table>
@@ -40,13 +44,13 @@ containing a value of this type.
         public static final int showBackground=0x7f010000;
     }
     public static final class color {
-        public static final int Grey=0x7f050002;
-        public static final int ORHS_Blue=0x7f050006;
-        public static final int SchoolColor1=0x7f050000;
-        public static final int SchoolColor2=0x7f050001;
-        public static final int SelectedColor=0x7f050004;
-        public static final int White=0x7f050003;
-        public static final int randomColor=0x7f050005;
+        public static final int Grey=0x7f070002;
+        public static final int ORHS_Blue=0x7f070006;
+        public static final int SchoolColor1=0x7f070000;
+        public static final int SchoolColor2=0x7f070001;
+        public static final int SelectedColor=0x7f070004;
+        public static final int White=0x7f070003;
+        public static final int randomColor=0x7f070005;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -60,74 +64,105 @@ containing a value of this type.
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f060000;
-        public static final int activity_vertical_margin=0x7f060001;
+        public static final int activity_horizontal_margin=0x7f080000;
+        public static final int activity_vertical_margin=0x7f080001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int orhs_background=0x7f020001;
-        public static final int orhs_drawable=0x7f020002;
-        public static final int orhs_drawable_funny=0x7f020003;
+        public static final int arrow_next=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int item_image=0x7f020002;
+        public static final int orhs_background=0x7f020003;
+        public static final int orhs_drawable=0x7f020004;
+        public static final int orhs_drawable_funny=0x7f020005;
     }
     public static final class id {
-        public static final int April=0x7f040003;
-        public static final int August=0x7f040007;
-        public static final int Calendar=0x7f04000e;
-        public static final int CalendarTitle=0x7f040017;
-        public static final int December=0x7f04000b;
-        public static final int February=0x7f040001;
-        public static final int InternetClause=0x7f040013;
-        public static final int January=0x7f040000;
-        public static final int July=0x7f040006;
-        public static final int June=0x7f040005;
-        public static final int MainCalendar=0x7f04000c;
-        public static final int March=0x7f040002;
-        public static final int May=0x7f040004;
-        public static final int November=0x7f04000a;
-        public static final int October=0x7f040009;
-        public static final int SelectionView=0x7f040015;
-        public static final int September=0x7f040008;
-        public static final int action_settings=0x7f040018;
-        public static final int container=0x7f040014;
-        public static final int myTextView=0x7f040016;
-        public static final int netToggleButton=0x7f040012;
-        public static final int newsfeed=0x7f04000f;
-        public static final int textView1=0x7f040011;
-        public static final int trojanGuy=0x7f04000d;
-        public static final int yearview=0x7f040010;
+        public static final int April=0x7f060003;
+        public static final int August=0x7f060007;
+        public static final int Calendar=0x7f06000e;
+        public static final int CalendarTitle=0x7f06001f;
+        public static final int December=0x7f06000b;
+        public static final int February=0x7f060001;
+        public static final int InternetClause=0x7f060013;
+        public static final int January=0x7f060000;
+        public static final int July=0x7f060006;
+        public static final int June=0x7f060005;
+        public static final int MainCalendar=0x7f06000c;
+        public static final int March=0x7f060002;
+        public static final int May=0x7f060004;
+        public static final int November=0x7f06000a;
+        public static final int October=0x7f060009;
+        public static final int SelectionView=0x7f060015;
+        public static final int September=0x7f060008;
+        public static final int action_settings=0x7f060020;
+        public static final int arrow=0x7f06001c;
+        public static final int container=0x7f060014;
+        public static final int date=0x7f06001d;
+        public static final int desc=0x7f060019;
+        public static final int listView=0x7f06001a;
+        public static final int myTextView=0x7f060016;
+        public static final int netToggleButton=0x7f060012;
+        public static final int newsfeed=0x7f06000f;
+        public static final int refresh=0x7f060022;
+        public static final int settings=0x7f060021;
+        public static final int sv=0x7f060018;
+        public static final int textView1=0x7f060011;
+        public static final int thumb=0x7f06001b;
+        public static final int title=0x7f060017;
+        public static final int trojanGuy=0x7f06000d;
+        public static final int webview=0x7f06001e;
+        public static final int yearview=0x7f060010;
     }
     public static final class layout {
         public static final int activity_cal=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_net=0x7f030002;
         public static final int activity_news=0x7f030003;
-        public static final int menu_cal=0x7f030004;
-        public static final int year_layout=0x7f030005;
+        public static final int detail=0x7f030004;
+        public static final int feed_list=0x7f030005;
+        public static final int list_item=0x7f030006;
+        public static final int main=0x7f030007;
+        public static final int menu_cal=0x7f030008;
+        public static final int year_layout=0x7f030009;
     }
     public static final class menu {
-        public static final int cal=0x7f090000;
-        public static final int main=0x7f090001;
-        public static final int net=0x7f090002;
-        public static final int news=0x7f090003;
-        public static final int year=0x7f090004;
+        public static final int cal=0x7f0b0000;
+        public static final int main=0x7f0b0001;
+        public static final int mainmenu=0x7f0b0002;
+        public static final int net=0x7f0b0003;
+        public static final int news=0x7f0b0004;
+        public static final int year=0x7f0b0005;
     }
     public static final class string {
-        public static final int Agreement_Text=0x7f07000d;
-        public static final int Attention=0x7f07000f;
-        public static final int Calendar=0x7f070004;
-        public static final int NewsFeed=0x7f070005;
-        public static final int ORHS_Motto=0x7f070009;
-        public static final int TBI=0x7f07000e;
-        public static final int YearView=0x7f07000a;
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int colorButton=0x7f070007;
-        public static final int hello_world=0x7f070002;
-        public static final int netAgreement=0x7f07000c;
-        public static final int title_activity_cal=0x7f070003;
-        public static final int title_activity_net=0x7f07000b;
-        public static final int title_activity_news=0x7f070006;
-        public static final int title_activity_year=0x7f070008;
+        public static final int Agreement_Text=0x7f090016;
+        public static final int Attention=0x7f090018;
+        public static final int Calendar=0x7f09000d;
+        public static final int NewsFeed=0x7f09000e;
+        public static final int ORHS_Motto=0x7f090012;
+        public static final int TBI=0x7f090017;
+        public static final int YearView=0x7f090013;
+        public static final int action_settings=0x7f09000a;
+        public static final int app_name=0x7f090009;
+        public static final int colorButton=0x7f090010;
+        public static final int connection_error=0x7f090007;
+        public static final int desc=0x7f090006;
+        public static final int hello_world=0x7f09000b;
+        public static final int lost_connection=0x7f090004;
+        public static final int netAgreement=0x7f090015;
+        /**   NetworkActivity 
+         */
+        public static final int page_title=0x7f090002;
+        public static final int refresh=0x7f090001;
+        /**   Menu items 
+         */
+        public static final int settings=0x7f090000;
+        public static final int title_activity_authetication_error=0x7f090019;
+        public static final int title_activity_cal=0x7f09000c;
+        public static final int title_activity_net=0x7f090014;
+        public static final int title_activity_news=0x7f09000f;
+        public static final int title_activity_year=0x7f090011;
+        public static final int updated=0x7f090003;
+        public static final int wifi_connected=0x7f090005;
+        public static final int xml_error=0x7f090008;
     }
     public static final class style {
         /** 
@@ -151,11 +186,14 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f0a0000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f0a0001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
     public static final class styleable {
         /** Attributes that can be used with a CalendarV.
