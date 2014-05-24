7c139ef50edc8f3a4291f9f84aad357095c319b2
Kevin Shim <kevinshimy@gmail.com>
2014-05-23 17:26:49 -0700

    add libs

--- AndroidDev/gen/com/example/androiddev/R.java
+++ AndroidDev/gen/com/example/androiddev/R.java
@@ -72,15 +72,15 @@ containing a value of this type.
     public static final class id {
         public static final int April=0x7f040003;
         public static final int August=0x7f040007;
-        public static final int Calendar=0x7f04000e;
+        public static final int Calendar=0x7f04000f;
         public static final int CalendarTitle=0x7f040017;
         public static final int December=0x7f04000b;
         public static final int February=0x7f040001;
-        public static final int InternetClause=0x7f040013;
+        public static final int InternetClause=0x7f040014;
         public static final int January=0x7f040000;
         public static final int July=0x7f040006;
         public static final int June=0x7f040005;
-        public static final int MainCalendar=0x7f04000c;
+        public static final int MainCalendar=0x7f04000d;
         public static final int March=0x7f040002;
         public static final int May=0x7f040004;
         public static final int November=0x7f04000a;
@@ -88,28 +88,31 @@ containing a value of this type.
         public static final int SelectionView=0x7f040015;
         public static final int September=0x7f040008;
         public static final int action_settings=0x7f040018;
-        public static final int container=0x7f040014;
+        public static final int container=0x7f04000c;
         public static final int myTextView=0x7f040016;
-        public static final int netToggleButton=0x7f040012;
-        public static final int newsfeed=0x7f04000f;
-        public static final int textView1=0x7f040011;
-        public static final int trojanGuy=0x7f04000d;
-        public static final int yearview=0x7f040010;
+        public static final int netToggleButton=0x7f040013;
+        public static final int newsfeed=0x7f040010;
+        public static final int textView1=0x7f040012;
+        public static final int trojanGuy=0x7f04000e;
+        public static final int yearview=0x7f040011;
     }
     public static final class layout {
-        public static final int activity_cal=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_net=0x7f030002;
-        public static final int activity_news=0x7f030003;
-        public static final int menu_cal=0x7f030004;
-        public static final int year_layout=0x7f030005;
+        public static final int activity_authetication_error=0x7f030000;
+        public static final int activity_cal=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_net=0x7f030003;
+        public static final int activity_news=0x7f030004;
+        public static final int fragment_authetication_error=0x7f030005;
+        public static final int menu_cal=0x7f030006;
+        public static final int year_layout=0x7f030007;
     }
     public static final class menu {
-        public static final int cal=0x7f090000;
-        public static final int main=0x7f090001;
-        public static final int net=0x7f090002;
-        public static final int news=0x7f090003;
-        public static final int year=0x7f090004;
+        public static final int authetication_error=0x7f090000;
+        public static final int cal=0x7f090001;
+        public static final int main=0x7f090002;
+        public static final int net=0x7f090003;
+        public static final int news=0x7f090004;
+        public static final int year=0x7f090005;
     }
     public static final class string {
         public static final int Agreement_Text=0x7f07000d;
