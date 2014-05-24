ed0e9bbd575bd290435c3cb867785a7fe3312027
Kevin Shim <kevinshimy@gmail.com>
2014-05-23 17:16:27 -0700

    Revert "Revert "Revert "what?"""
    This reverts commit 9358cb5220a22f2a4fd09225e07578b716a5ae8a.

---
+++ AndroidDev/src/com/example/androiddev/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.androiddev;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+
+
+public class MainActivity extends Activity {
+	public static final String netFile="NET_AGREE_FILE";
+	public static final String netSigned="NET_AGREE_BOOLEAN";
+	private int smiley;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		smiley=0;
+		((ImageView)findViewById(R.id.trojanGuy)).setSoundEffectsEnabled(false);;
+	}
+	protected void onResume(){
+		super.onResume();
+		SharedPreferences myPrefs=this.getSharedPreferences(netFile, 0);
+		boolean netSignedBool=myPrefs.getBoolean(netSigned, false);
+		if(!netSignedBool)
+		{
+			try{
+			Intent intent=new Intent(this, NetActivity.class);
+			startActivity(intent);
+			}
+			catch(Exception e){}
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	public void toCalendar(View view){
+		Intent intent=new Intent(this, CalActivity.class);
+		startActivity(intent);
+	}
+	public void toNewsFeed(View view){
+		Intent intent=new Intent(this, NewsActivity.class);
+		startActivity(intent);
+	}
+	public void toYearView(View view){
+		Intent intent=new Intent(this, Year.class);
+		startActivity(intent);
+	}
+	public void smileyCounter(View view){
+		smiley++;
+		if(smiley==30)
+		{
+			ImageView trojan=(ImageView)findViewById(R.id.trojanGuy);
+			trojan.setImageResource(R.drawable.orhs_drawable_funny);
+		}
+	}
+}
---
+++ AndroidDev/src/com/example/androiddev/SelectionView.java
@@ -0,0 +1,114 @@
+package com.example.androiddev;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
+
+public class SelectionView extends View implements OnTouchListener{
+	private float intervalY; 
+	private ArrayList<Integer> mColors;
+	private ArrayList<Integer>mSelectedColors;
+	private ArrayList<RectF> mColorBoxes;
+	private Paint mPaint;
+	
+	public SelectionView(Context context, AttributeSet attr){
+		super(context, attr);
+		this.setOnTouchListener(this);
+		mColors=new ArrayList<Integer>();
+		mSelectedColors=new ArrayList<Integer>();
+		mColorBoxes=new ArrayList<RectF>();
+		mPaint=new Paint();
+	}
+	public void addColors(ArrayList<Integer> colors)
+	{
+		for(int i=0; i<colors.size(); i++)
+		{
+			if(!mColors.contains(colors.get(i)))
+				mColors.add(colors.get(i));
+		}
+		createBoxes();
+		invalidate();
+		this.requestLayout();
+	}
+	private void createBoxes(){
+		mColorBoxes.clear();
+		for(int i=0; i<mColors.size();i++)
+		{
+			RectF myRect=new RectF(0,i*intervalY, getWidth(),(i+1)*intervalY);
+			mColorBoxes.add(myRect);
+		}
+	}
+	public void onSizeChanged(int w, int h, int oldw, int oldh){
+		super.onSizeChanged(w,h,oldw,oldh);
+		intervalY=getHeight()/mColors.size();
+		createBoxes();
+	}
+	public void drawBox(RectF box,int color, boolean isSelected, Canvas canvas){
+		if(isSelected)
+			mPaint.setColor(getResources().getColor(R.color.SelectedColor));
+		else
+			mPaint.setColor(getResources().getColor(R.color.White));
+		canvas.drawRect(box, mPaint);
+		mPaint.setColor(color);
+		canvas.drawRect((box.left+2.5f),(box.top+2.5f), (box.right-2.5f), (box.bottom-2.5f), mPaint);
+	}
+	public void onDraw(Canvas canvas)
+	{
+		for(int i=0; i<mColors.size();i++)
+		{
+			if(mSelectedColors.contains(mColors.get(i)))
+			{
+				drawBox(mColorBoxes.get(i),mColors.get(i),true,canvas);
+			}
+			else
+				drawBox(mColorBoxes.get(i),mColors.get(i),false,canvas);
+		}
+	}
+	public boolean onTouch (View v, MotionEvent event) {
+		 float x=event.getX();
+		 float y=event.getY();
+		 if(event.getAction()==MotionEvent.ACTION_DOWN)
+		 for(int i=0; i<mColorBoxes.size();i++)
+		 {
+			 if(mColorBoxes.get(i).contains(x, y) && !mSelectedColors.contains(mColors.get(i)))
+			 {
+				 mSelectedColors.add(mColors.get(i));
+				 this.invalidate((int)(mColorBoxes.get(i).left),(int)mColorBoxes.get(i).top, (int)mColorBoxes.get(i).right, (int)mColorBoxes.get(i).bottom);
+				 
+				 requestLayout();
+				 requestChange();
+				 return true;
+			 }
+			 else
+				 if(mColorBoxes.get(i).contains(x, y) && mSelectedColors.contains(mColors.get(i)))
+				 {
+					 mSelectedColors.remove(mSelectedColors.indexOf(mColors.get(i)));
+					 this.invalidate((int)(mColorBoxes.get(i).left),(int)mColorBoxes.get(i).top, (int)mColorBoxes.get(i).right, (int)mColorBoxes.get(i).bottom);
+					
+					 requestLayout();
+					 requestChange();
+					 return true;
+				 }
+			 
+		 }
+		 return false;
+	 }
+	public void requestChange()
+	{
+		NewsActivity host=(NewsActivity)getContext();
+		host.getSelectedColors(null);
+	}
+	public ArrayList<Integer> getSelectedColors()
+	{
+		return mSelectedColors;
+	}
+	 
+}
