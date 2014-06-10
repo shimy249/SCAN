92a4b02f1ee71894d857030a5463787458b6a0af
Kevin Shim <kevinshimy@gmail.com>
2014-06-09 20:07:14 -0700

    Final fix for build path

--- AndroidDev/src/com/ellume/scan/Event.java
+++ AndroidDev/src/com/ellume/scan/Event.java
@@ -2,7 +2,13 @@ package com.ellume.scan;
 
 import java.util.Date;
 
+import android.util.Log;
+
+import com.google.api.client.util.DateTime;
+
 public class Event {
+	public static final String FIELDS = "items(id,summary,description,start,end)";
+	private String id;
 	private String name;
 	private String summary;
 	private Date startDate;
@@ -15,6 +21,27 @@ public Event(String n, String s, Date start, Date end,int $color){
 	startDate = start;
 	endDate = end;
 	color=$color;
+}
+
+public Event(com.google.api.services.calendar.model.Event current) {
+	id = current.getId();
+	name = current.getSummary(); //title
+	summary = current.getDescription(); //description or summary whatever
+	//startDate = new Date(current.getStart().getDateTime().getValue());
+	//endDate = new Date(current.getEnd().getDateTime().getValue());
+		
+	//int startIndex = name.indexOf("[");
+	//int endIndex = name.indexOf("]");
+	//startIndex +=1;
+	
+	//String category = name.substring(startIndex,endIndex);
+	
+	
+	
+}
+
+public String getId(){
+	return id;
 }
 public Date getStartDate()
 {
